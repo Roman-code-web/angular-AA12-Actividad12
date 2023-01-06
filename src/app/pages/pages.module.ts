23bb11ae2dbfea266bd9ee446f6786e960145510
Roman-code-web <selenerg.275@gmail.com>
2023-01-06 11:03:16 -0500

    solucion de error al hacer dploy

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -3,17 +3,22 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { SharedModule } from '../shared/shared.module';
 import { ComponentesModule } from '../componentes/componentes.module';
+import { ValidacionDirective } from '../directivas/validacion.directive';
 
 
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    ValidacionDirective
   ],
   imports: [
     CommonModule,
     ComponentesModule,
+  ],exports:[
+    HomeComponent,
+    ValidacionDirective
   ]
 })
 export class PagesModule { }
